7c82fe32cbee47176f2e7477e7a968afb0991739
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-04-01 16:35:00 -0400

    Add number generator and input reader

--- guessingGame.js
+++ guessingGame.js
@@ -1,10 +1,4 @@
-/* **** Global Variables **** */
-// try to elminate these global variables in your project, these are here just to start.
-
-var playersGuess,
-    winningNumber
-
-
+var playersGuess, winningNumber;
 
 /* **** Guessing Game Functions **** */
 
@@ -12,24 +6,45 @@ var playersGuess,
 
 function generateWinningNumber(){
 	// add code here
+  var min = 1, max =100;
+  return winningNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
 }
 
 // Fetch the Players Guess
 
 function playersGuessSubmission(){
 	// add code here
+  playersGuess = +$("#guess").val();
+  //console.log(typeof playersGuess);
+  $("#guess").val("");
+  return checkGuess();
 }
 
 // Determine if the next guess should be a lower or higher number
 
 function lowerOrHigher(){
 	// add code here
+  alert ("Oh well");
+
+
+
 }
 
-// Check if the Player's Guess is the winning number 
+// Check if the Player's Guess is the winning number
 
 function checkGuess(){
 	// add code here
+  console.log(winningNumber + " " + playersGuess);
+  if(playersGuess===winningNumber){
+    alert("Winner, winner, chicken dinner!");
+
+  }else{
+    
+
+  }
+
+  playAgain();
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
@@ -46,4 +61,12 @@ function playAgain(){
 
 
 /* **** Event Listeners/Handlers ****  */
+//on document load
+$(document).ready(function() {
+  generateWinningNumber();
 
+  $("#submit").click(function() {
+    playersGuessSubmission();
+  });
+
+});
